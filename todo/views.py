23c358443f988d45bf742275e653686924d9757f
Shell2016 <bvg2003@bk.ru>
2020-05-30 04:07:41 +0300

    second commit

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from .models import Todo
+from .forms import TodoForm
 
 
 def index(request):
-    return render(request, 'todo/index.html')
+    todo_list = Todo.objects.all()
+    form = TodoForm()
+    context = {'todo_list': todo_list, 'form': form}
+    return render(request, 'todo/index.html', context)
+
+
+@require_POST
+def addtodo(request):
+    form = TodoForm(request.POST)
+    if form.is_valid:
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
